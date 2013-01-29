fbc03c071a46e13fa418991b3e3111c717e69c14
Natan Žabkar <natan.zabkar@gmail.com>
2013-01-29 22:56:25 +0100

    Player can move and changes images appropriately, tested in main

--- v1.0/game/player.py
+++ v1.0/game/player.py
@@ -1,7 +1,41 @@
 import pyglet
+from pyglet.window import key
+from .screen import *
+from . import resources
 
 
 class Player(pyglet.sprite.Sprite):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(img = resources.player1_normal, *args, **kwargs)
+        
+        #self.image = 
+        self.x = game_window.width/2 - self.width/2
+        self.y = window_bottom
+        
+        self.speed = 300
+        self.vx = 0
+        self.key_handler = key.KeyStateHandler()
+        
+    def update(self, dt):
+        self.keys()
+        self.changeImg()
+        self.move(dt)
+        
+    def move(self, dt):
+        self.x += self.vx * dt
+            
+    def keys(self):
+        self.vx = 0
+        if(self.key_handler[key.LEFT]):
+            self.vx -= self.speed
+        if(self.key_handler[key.RIGHT]):
+            self.vx += self.speed
+    
+    def changeImg(self):
+        if(self.vx < 0):
+            self.image = resources.player1_left
+        elif(self.vx > 0):
+            self.image = resources.player1_right
+        elif(self.vx == 0):
+            self.image = resources.player1_normal
         
--- v1.0/main.pyw
+++ v1.0/main.pyw
@@ -1,15 +1,21 @@
 import pyglet
 from game.screen import *
+from game import player
 
 
 @game_window.event
 def on_draw():
     game_window.clear()
+    ply.draw()
     
     
 def update(dt):
-    pass
+   ply.update(dt)
+
+
+ply = player.Player()
+game_window.push_handlers(ply.key_handler)
 
 if(__name__ == '__main__'):
-    pyglet.clock.schedule_interval(update, 1/120)
-    pyglet.app.run()
+  pyglet.clock.schedule_interval(update, 1/120)
+  pyglet.app.run()
