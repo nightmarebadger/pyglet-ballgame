f3b15ed9888435be162df09bd64abd0dff7f2285
Natan Žabkar <natan.zabkar@gmail.com>
2013-01-29 23:04:41 +0100

    Added stuff to resource.py

--- v1.0/game/resources.py
+++ v1.0/game/resources.py
@@ -1,5 +1,10 @@
 import pyglet
 
+
+#===============================================================================
+# PLAYER IMAGES
+#===============================================================================
+
 pyglet.resource.path = ["../resources/images/player"]
 pyglet.resource.reindex()
 
@@ -8,8 +13,31 @@ player1_normal = pyglet.resource.image("ply1n.png")
 player1_right = pyglet.resource.image("ply1r.png")
 player1_shooting = pyglet.resource.image("ply1s.png")
 
+player2_left = pyglet.resource.image("ply2l.png")
+player2_normal = pyglet.resource.image("ply2n.png")
+player2_right = pyglet.resource.image("ply2r.png")
+player2_shooting = pyglet.resource.image("ply2s.png")
+
+#===============================================================================
+# BALL IMAGES
+#===============================================================================
+
+pyglet.resource.path = ["../resources/images/ball"]
+pyglet.resource.reindex()
+
+ball_blue = pyglet.resource.image("blue.png")
+ball_gold = pyglet.resource.image("gold.png")
+ball_green = pyglet.resource.image("green.png")
+ball_red = pyglet.resource.image("red.png")
+
+#===============================================================================
+# BACKGROUND IMAGES
+#===============================================================================
 
 pyglet.resource.path = ["../resources/images/background"]
 pyglet.resource.reindex()
 
-background_image1 = pyglet.resource.image("1.png")
+background1 = pyglet.resource.image("1.png")
+background2 = pyglet.resource.image("2.png")
+background3 = pyglet.resource.image("3.png")
+background4 = pyglet.resource.image("4.png")
--- v1.0/game/screen.py
+++ v1.0/game/screen.py
@@ -4,4 +4,4 @@ from . import resources
 game_window = pyglet.window.Window(width=800, height=650, caption="Pyglet Ballgame")
 window_bottom = 50
 
-background = pyglet.sprite.Sprite(img = resources.background_image1)
+background = pyglet.sprite.Sprite(img = resources.background1)
